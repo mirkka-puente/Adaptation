17b92b4ce715742976edf3bc2506009326afda24
mirkka29 <mirkka_92@alumni.ubc.ca>
2022-12-31 15:31:35 +0100

    commit for the day

--- R/05_Bonferroni-beta-sonchus.R
+++ R/05_Bonferroni-beta-sonchus.R
@@ -32,42 +32,55 @@ dt1 <- mutate(dt1,
 ### Neat data to work on
 
 parameters <- c("Week", 'Date','Species', 'PlantId', 'Use', 'Treatment',
-                "Leaf_number","Aerial_dry_weight",
-                "Roots_fresh_weight", "Aerial_water_content")
+                "Leaf_number","Chlorophyll_content", "Root_water_content")
 
-num.var <- c("Leaf_number","Aerial_dry_weight",
-             "Roots_fresh_weight", "Aerial_water_content")
+num.var <- c("Leaf_number","Chlorophyll_content", "Root_water_content")
 
 
 w <- "W6"
 
 dt2 <- dt1 %>% filter(Week == w) %>% select(all_of(parameters))
 
+dt.beta <- dt2 %>% filter(Species == "Beta vulgaris")
+
+dt.sonchus <- dt2 %>% filter(Species == "Sonchus oleraceus")
+
+dt3 <- bind_rows(dt.beta, dt.sonchus)
+
+rm(dt.beta, dt.sonchus, dt2)
+
 
 ### Bonferroni test-----
 
 # Creating the table to keep the information 
 groups <- c("Control.Intermediate", "Control.Stress", "Intermediate.Stress")
+species <- c("Beta vulgaris", "Sonchus oleraceus")
 col_nam <- c("Species","Variables", groups)
 num_col <- length(col_nam)
-num_row <- length(levels(dt2$Species))  * length(num.var)
+num_row <- length(species)  * length(num.var)
+
+#Bonferroni table
 b_table <- as.data.frame(matrix(nrow = num_row, ncol = num_col))
 names(b_table) <- col_nam
 
+#result table
+res_table <- as.data.frame(matrix(nrow = num_row, ncol = num_col))
+names(res_table) <- col_nam
+
 # Variables for the loop
-sp <- levels(dt2$Species)[1]
+sp <- species[1]
 nv <- num.var[1]
 i <- 1
 
 
-for(sp in levels(dt2$Species)){
+for(sp in species){
   for(nv in num.var){
     # Data filtered by species
-    dt3 <- dt2 %>% filter(Species == sp) %>% select(nv,"Treatment")
+    dt4 <- dt3 %>% filter(Species == sp) %>% select(nv,"Treatment")
     
     # Bonferroni test
     
-    bt <- pairwise.t.test(dt3[[1]], dt3$Treatment, p.adjust.method="bonferroni")
+    bt <- pairwise.t.test(dt4[[1]], dt4$Treatment, p.adjust.method="bonferroni")
     
     # Allocating the values in the table
     b_table$Species[i] <- sp
@@ -77,45 +90,8 @@ for(sp in levels(dt2$Species)){
     n3 <- round(bt$p.value[4], 3)
     vec <- c(n1, n2, n3)
     b_table[i, groups] <- vec
-      
-    i <- i + 1
     
-    # Remove the data just in case
-    rm(dt3, n1, n2, n3, bt, vec)
-  }
-}
-
-# Remove variables for future
-rm(i, nv, sp)
-
-### Summary table Bonferroni --------------------------------------
- 
-res_table <- as.data.frame(matrix(nrow = num_row, ncol = num_col))
-names(res_table) <- col_nam
-
-# Variables for the loop
-sp <- levels(dt2$Species)[1]
-nv <- num.var[1]
-i <- 1
-
-
-for(sp in levels(dt2$Species)){
-  for(nv in num.var){
-    # Data filtered by species
-    dt3 <- dt2 %>% filter(Species == sp) %>% select(nv,"Treatment")
-    
-    # Bonferroni test
-    
-    bt <- pairwise.t.test(dt3[[1]], dt3$Treatment, p.adjust.method="bonferroni")
-    
-    # Allocating the values in the table
-    res_table$Species[i] <- sp
-    res_table$Variables[i] <- nv
-    
-    n1 <- round(bt$p.value[1], 3)
-    n2 <- round(bt$p.value[2], 3)
-    n3 <- round(bt$p.value[4], 3)
-    vec <- c(n1, n2, n3)
+    #Results
     sig <- c()
     
     for(n in vec){
@@ -127,14 +103,15 @@ for(sp in levels(dt2$Species)){
     }
     
     res_table[i, groups] <- sig
-    
+      
     i <- i + 1
     
     # Remove the data just in case
-    rm(dt3, n1, n2, n3, bt, vec, sig)
+    rm(dt4, n1, n2, n3, bt, vec, sig)
   }
 }
 
 # Remove variables for future
-rm(i, nv, sp)
+rm(i, nv, sp, n)
+
 
